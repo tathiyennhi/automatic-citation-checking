f5f880450d7db5439169a5e33b63e12325941023
Nhi Ta <tathiyennhi@gmail.com>
2024-10-23 09:34:29 +0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#windows
+python -m venv venv
+.\venv\Scripts\activate
+pip install -r requirements.txt
+python <your_main_file.py>
+
+#macos
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python <your_main_file.py>
