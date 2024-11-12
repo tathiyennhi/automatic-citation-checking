ed182759da7d5a6caa24b3faf1d101c432fe1b45
Nhi Ta <tathiyennhi@gmail.com>
2024-11-12 10:56:22 +0700

    update env path

--- run.sh
+++ run.sh
@@ -9,3 +9,9 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python <your_main_file.py>
+
+# Tạo môi trường ảo trên ổ D: (Windows)
+python -m venv D:/myenv
+
+# Tạo môi trường ảo trong thư mục người dùng (Mac)
+python -m venv /Users/tathiyennhi/myenv
